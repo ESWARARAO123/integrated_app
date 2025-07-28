a710eebb1ef8d2baa2c790e29a94874b813f76f9
Yaswanth-Sierraedge-Ai <yaswanth@sierraedge.ai>
2025-07-28 06:50:35 +0000

    fixed permission issues for the DATA folder

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -14,6 +14,18 @@ mkdir -p /app/DATA/documents
 
 # Set permissions
 chmod -R 755 /app/python/RAG-MODULE
+chmod -R 755 /app/DATA/
+chmod -R 755 /app/DATA/embeddings
+chmod -R 755 /app/DATA/vector_store
+chmod -R 755 /app/DATA/chroma_data
+chmod -R 755 /app/DATA/input
+chmod -R 755 /app/DATA/output
+chmod -R 755 /app/DATA/collections
+chmod -R 755 /app/DATA/uploads
+
+
+
+
 
 # Execute the command passed to the script
 exec "$@" 
